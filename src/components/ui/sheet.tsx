b8d0ecd296f1338177811434e8568628b21bc213
[dyad] <saisatyapavanpanduvanka@my.unt.edu>
2025-08-08 20:31:59 -0500

    [dyad] wrote 1 file(s)

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -58,7 +58,7 @@ const SheetContent = React.forwardRef<
   SheetContentProps
 >(({ side = "right", className, children, ...props }, ref) => (
   <SheetPortal>
-    <SheetOverlay />
+    {/* Removed SheetOverlay to prevent faded background */}
     <SheetPrimitive.Content
       ref={ref}
       className={cn(sheetVariants({ side }), className)}
